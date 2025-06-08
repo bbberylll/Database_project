d59ed4d786a2a9fbcf776c126175c8646f5e0bd4
rachelses <esshin0122@gmail.com>
2025-06-08 22:29:06 +0900

    Add files via upload

---
+++ DB_Final/SQL/drop_table.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS payment;
+DROP TABLE IF EXISTS ticket;
+DROP TABLE IF EXISTS reservation;
+DROP TABLE IF EXISTS seat;
+DROP TABLE IF EXISTS train_schedule;
+DROP TABLE IF EXISTS train;
+DROP TABLE IF EXISTS passenger;
+DROP TABLE IF EXISTS station;
+
+DROP VIEW IF EXISTS available_seat_view;
+DROP VIEW IF EXISTS destination_passenger_summary_view;
+DROP VIEW IF EXISTS detailed_train_schedule_view;
